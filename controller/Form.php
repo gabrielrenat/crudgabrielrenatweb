271760477f21c3a400e800547bfc06f7def50b7b
Maycon <mayconsousa91@outlook.com>
2022-04-19 08:35:30 -0300

    Aula do dia 19/04/2022

--- controller/Form.php
+++ controller/Form.php
@@ -4,8 +4,12 @@ class Form
   private $message = "";
   public function controller()
   {
-    $form = new Template ("view/form.html");
+    $form = new Template("view/form.html");
     $this->message = $form->saida();
+  }
+  public function salvar()
+  {
+    print_r($_POST);
   }
   public function getMessage()
   {
