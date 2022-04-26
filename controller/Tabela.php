2662039e06bf89d32dfe70cfae7d0f1043f27358
Maycon <mayconsousa91@outlook.com>
2022-04-26 08:01:17 -0300

    Aula do dia 26/04

--- controller/Tabela.php
+++ controller/Tabela.php
@@ -3,7 +3,11 @@ class Tabela
 {
   private $message = "";
   public function controller()
-  {
+  { 
+    Transaction::get();
+    $livro = new Crud("livro");
+    $resultado = $livro->select();
+    print_r($resultado);
     $this->message = "Estou na classe Tabela";
   }
   public function getMessage()
