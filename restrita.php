9602df435447e290aa2e77d4643b65073985a36e
Maycon <mayconsousa91@outlook.com>
2022-05-10 08:33:56 -0300

    Aula do dia 10/05/2022

---
+++ restrita.php
@@ -0,0 +1,51 @@
+<?php
+// auto load
+spl_autoload_extensions('.php');
+function classLoader($class)
+{
+  $pastas = array('controller', 'model');
+  foreach ($pastas as $pasta) {
+    $arquivo = "{$pasta}/{$class}.php";
+    if (file_exists($arquivo)) {
+      require_once($arquivo);
+    }
+  }
+}
+spl_autoload_register("classLoader");
+
+Session::startSession();
+if (!Session::getValue('id')) {
+  header("Location:" . Aplicacao::$app . "/");
+}
+
+// Front Controller
+class Aplicacao
+{
+  static public $app = "/lazaro2";
+  public static function run()
+  {
+    $layout = new Template('view/layout.html');
+    $method = "";
+    if (isset($_GET["class"])) {
+      $class = $_GET["class"];
+    }
+    if (isset($_GET["method"])) {
+      $method = $_GET["method"];
+    }
+    if (empty($class)) {
+      $class = "Inicio";
+    }
+    if (class_exists($class)) {
+      $pagina = new $class();
+      if (method_exists($pagina, $method)) {
+        $pagina->$method();
+      } else {
+        $pagina->controller();
+      }
+      $layout->set('uri', self::$app);
+      $layout->set('conteudo', $pagina->getMessage());
+    }
+    echo $layout->saida();
+  }
+}
+Aplicacao::run();
